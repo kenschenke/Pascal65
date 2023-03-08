3c396bcd99233a59c083566178fe0cbc28c272d0
Ken Schenke <kenschenke@gmail.com>
2023-03-08 09:02:36 -0600

    Reduce memory footprint for error message handling

--- src/shared/error.c
+++ src/shared/error.c
@@ -19,27 +19,28 @@
 #include <common.h>
 #include <string.h>
 #include <chunks.h>
+#include <ctype.h>
 
 unsigned errorCount = 0;
 unsigned errorArrowFlag = 1;
 unsigned errorArrowOffset = 8;
 
-static CHUNKNUM abortMsg[numAbortErrors];
-static CHUNKNUM errorMessages[numParserErrors];
-static CHUNKNUM runtimeErrorMessages[numRuntimeErrors];
+static CHUNKNUM abortMsg;
+static CHUNKNUM errorMessages;
+static CHUNKNUM runtimeErrorMessages;
 
 static char msgbuf[CHUNK_LEN + 1];
 
-static void getMessage(CHUNKNUM chunkNum);
+static void getMessage(CHUNKNUM chunkNum, int msgNum);
 static void readErrorFile(char *filename, CHUNKNUM *msgs, int numMsgs);
 
 void abortTranslation(TAbortCode ac)
 {
-    if (!abortMsg[0]) {
-        readErrorFile("abortmsgs", abortMsg, numAbortErrors);
+    if (!abortMsg) {
+        readErrorFile("abortmsgs", &abortMsg, numAbortErrors);
     }
 
-    getMessage(abortMsg[-ac - 1]);
+    getMessage(abortMsg, (int) (-ac) - 1);
     logFatalError(msgbuf);
     isFatalError = 1;
 #if 1
@@ -51,28 +52,34 @@ void Error(TErrorCode ec)
 {
     const int maxSyntaxErrors = 25;
 
-    if (!errorMessages[0]) {
-        readErrorFile("errormsgs", errorMessages, numParserErrors);
+    if (!errorMessages) {
+        readErrorFile("errormsgs", &errorMessages, numParserErrors);
     }
 
-    getMessage(errorMessages[ec]);
+    getMessage(errorMessages, (int) ec);
     logError(msgbuf, currentLineNumber, ec);
     if (++errorCount > maxSyntaxErrors) {
         abortTranslation(abortTooManySyntaxErrors);
     }
 }
 
-static void getMessage(CHUNKNUM chunkNum)
+static void getMessage(CHUNKNUM chunkNum, int msgNum)
 {
     char chunk[CHUNK_LEN];
 
-    retrieveChunk(chunkNum, (unsigned char *)chunk);
+    setMemBufPos(chunkNum, msgNum * CHUNK_LEN);
+    readFromMemBuf(chunkNum, chunk, CHUNK_LEN);
     sprintf(msgbuf, "%.*s", CHUNK_LEN, chunk);
 }
 
 static void readErrorFile(char *filename, CHUNKNUM *msgs, int numMsgs) {
     FILE *fp;
-    int i, x;
+    int i;
+    char buf[40];
+
+    allocMemBuf(msgs);
+    reserveMemBuf(*msgs, numMsgs * CHUNK_LEN);
+    setMemBufPos(*msgs, 0);
 
     fp = fopen(filename, "r");
     if (fp == NULL) {
@@ -82,33 +89,25 @@ static void readErrorFile(char *filename, CHUNKNUM *msgs, int numMsgs) {
     }
 
     for (i = 0; i < numMsgs; ++i) {
-        x = 0;
-        memset(msgbuf, 0, CHUNK_LEN);
-        while (1) {
-            if (x > CHUNK_LEN) {
-                logFatalError("Error message too long");
-                isFatalError = 1;
-                fclose(fp);
-                return;
-            }
-
-            if (fread(msgbuf + x, 1, 1, fp) != 1) {
-                logFatalError("Error reading from error message file");
-                isFatalError = 1;
-                fclose(fp);
-                return;
-            }
-
-            if (msgbuf[x] == '\r') {
-                msgbuf[x] = 0;
-                break;
-            }
-
-            ++x;
+        if (!fgets(buf, sizeof(buf), fp)) {
+            logFatalError("Error reading from error message file");
+            isFatalError = 1;
+            fclose(fp);
+            return;
         }
 
-        allocChunk(msgs + i);
-        storeChunk(msgs[i], (unsigned char *)msgbuf);
+        while (isspace(buf[strlen(buf)-1])) {
+            buf[strlen(buf)-1] = 0;
+        }
+
+        if (strlen(buf) > CHUNK_LEN) {
+            logFatalError("Error message too long");
+            isFatalError = 1;
+            fclose(fp);
+            return;
+        }
+
+        writeToMemBuf(*msgs, buf, CHUNK_LEN);
     }
 
     fclose(fp);
@@ -116,11 +115,11 @@ static void readErrorFile(char *filename, CHUNKNUM *msgs, int numMsgs) {
 
 void runtimeError(TRuntimeErrorCode ec)
 {
-    if (!runtimeErrorMessages[0]) {
-        readErrorFile("runtimemsgs", runtimeErrorMessages, numRuntimeErrors);
+    if (!runtimeErrorMessages) {
+        readErrorFile("runtimemsgs", &runtimeErrorMessages, numRuntimeErrors);
     }
 
-    getMessage(runtimeErrorMessages[ec]);
+    getMessage(runtimeErrorMessages, (int) ec);
     logRuntimeError(msgbuf, currentLineNumber);
     isFatalError = 1;
 }
