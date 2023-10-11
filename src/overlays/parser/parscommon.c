e2c4a4aa9771d56133c848c23d63b484e8b7ccf5
Ken Schenke <kenschenke@gmail.com>
2023-10-11 11:56:59 -0500

    Move token lists to parser (from common)

---
+++ src/overlays/parser/parscommon.c
@@ -0,0 +1,182 @@
+#include <misc.h>
+
+// Tokens that can start a statement.
+extern const TTokenCode tlStatementStart[] = {
+    tcBEGIN, tcCASE, tcFOR, tcIF, tcREPEAT, tcWHILE, tcIdentifier,
+    tcDummy
+};
+
+// Tokens that can follow a statement
+extern const TTokenCode tlStatementFollow[] = {
+    tcSemicolon, tcPeriod, tcEND, tcELSE, tcUNTIL, tcDummy
+};
+
+// Tokens that cannot occur outside a statement such as IF
+extern const TTokenCode tlStatementListNotAllowed[] = {
+    tcELSE, tcDummy
+};
+
+// Tokens that can start a CASE label
+extern const TTokenCode tlCaseLabelStart[] = {
+    tcIdentifier, tcNumber, tcPlus, tcMinus, tcString, tcDummy
+};
+
+// Tokens that can start an expression
+extern const TTokenCode tlExpressionStart[] = {
+    tcPlus, tcMinus, tcIdentifier, tcNumber, tcString,
+    tcNOT, tcLParen, tcDummy
+};
+
+// Tokens that can follow an expression
+extern const TTokenCode tlExpressionFollow[] = {
+    tcComma, tcRParen, tcRBracket, tcColon, tcTHEN, tcTO, tcDOWNTO,
+    tcDO, tcOF, tcDummy
+};
+
+// Relational operators
+extern const TTokenCode tlRelOps[] = {
+    tcEqual, tcNe, tcLt, tcGt, tcLe, tcGe, tcDummy
+};
+
+// Unary + and - operators
+extern const TTokenCode tlUnaryOps[] = {
+    tcPlus, tcMinus, tcDummy
+};
+
+// Additive operators
+extern const TTokenCode tlAddOps[] = {
+    tcPlus, tcMinus, tcOR, tcDummy
+};
+
+// Multiplicative operators
+extern const TTokenCode tlMulOps[] = {
+    tcStar, tcSlash, tcDIV, tcMOD, tcAND, tcDummy
+};
+
+// Tokens that can end a program
+extern const TTokenCode tlProgramEnd[] = {
+    tcPeriod, tcDummy
+};
+
+/// Tokens that can start a declaration
+const TTokenCode tlDeclarationStart[] = {
+    tcCONST, tcTYPE, tcVAR, tcPROCEDURE, tcFUNCTION, tcDummy
+};
+
+// Tokens that can follow a declaration
+const TTokenCode tlDeclarationFollow[] = {
+    tcSemicolon, tcIdentifier, tcDummy
+};
+
+// Tokens that can start an identifier or field
+const TTokenCode tlIdentifierStart[] = {
+    tcIdentifier, tcDummy
+};
+
+// Tokens that can follow an identifier or field
+const TTokenCode tlIdentifierFollow[] = {
+    tcComma, tcIdentifier, tcColon, tcSemicolon, tcDummy
+};
+
+// Tokens that can follow an identifier or field sublist
+const TTokenCode tlSublistFollow[] = {
+    tcColon, tcDummy
+};
+
+// Tokens that can follow a field declaration
+const TTokenCode tlFieldDeclFollow[] = {
+    tcSemicolon, tcIdentifier, tcEND, tcDummy
+};
+
+// Tokens that can start an enumeration constant
+const TTokenCode tlEnumConstStart[] = {
+    tcIdentifier, tcDummy
+};
+
+// Tokens that can follow an enumeration constant
+const TTokenCode tlEnumConstFollow[] = {
+    tcComma, tcIdentifier, tcRParen, tcSemicolon, tcDummy
+};
+
+// Tokens that can follow a subrange limit
+const TTokenCode tlSubrangeLimitFollow[] = {
+    tcDotDot, tcIdentifier, tcPlus, tcMinus, tcString,
+    tcRBracket, tcComma, tcSemicolon, tcOF, tcDummy
+};
+
+// Tokens that can start an index type
+const TTokenCode tlIndexStart[] = {
+    tcIdentifier, tcNumber, tcString, tcLParen, tcPlus, tcMinus,
+    tcDummy
+};
+
+// Tokens that can follow an index type
+const TTokenCode tlIndexFollow[] = {
+    tcComma, tcRBracket, tcOF, tcSemicolon, tcDummy
+};
+
+// Tokens that can follow an index type list
+const TTokenCode tlIndexListFollow[] = {
+    tcOF, tcIdentifier, tcLParen, tcARRAY, tcRECORD,
+    tcPlus, tcMinus, tcNumber, tcString, tcSemicolon, tcDummy
+};
+
+// Tokens that can start a procedure or function definition
+const TTokenCode tlProcFuncStart[] = {
+    tcPROCEDURE, tcFUNCTION, tcDummy
+};
+
+// Tokens that can follow a procedure or function definition
+const TTokenCode tlProcFuncFollow[] = {
+    tcSemicolon, tcDummy
+};
+
+// Tokens that can follow a routine header
+const TTokenCode tlHeaderFollow[] = {
+    tcSemicolon, tcDummy
+};
+
+// Tokens that can follow a function id in a header
+const TTokenCode tlProgProcIdFollow[] = {
+    tcLParen, tcColon, tcSemicolon, tcDummy
+};
+
+// Tokens that can follow a function id in a header
+const TTokenCode tlFuncIdFollow[] = {
+    tcLParen, tcColon, tcSemicolon, tcDummy
+};
+
+// Tokens than can follow an actual variable parameter
+const TTokenCode tlActualVarParmFollow[] = {
+    tcComma, tcRParen, tcDummy
+};
+
+// Tokens that can follow a formal parameter list
+const TTokenCode tlFormalParmsFollow[] = {
+    tcRParen, tcSemicolon, tcDummy
+};
+
+const TTokenCode tlSubscriptOrFieldStart[] = {
+    tcLBracket, tcPeriod, tcDummy
+};
+
+// Individual tokens
+extern const TTokenCode tlColonEqual[] = {tcColonEqual, tcDummy};
+extern const TTokenCode tlDO[] = {tcDO, tcDummy};
+extern const TTokenCode tlTHEN[] = {tcTHEN, tcDummy};
+extern const TTokenCode tlTODOWNTO[] = {tcTO, tcDOWNTO, tcDummy};
+extern const TTokenCode tlOF[] = {tcOF, tcDummy};
+extern const TTokenCode tlColon[] = {tcColon, tcDummy};
+extern const TTokenCode tlEND[] = {tcEND, tcDummy};
+
+char tokenIn(TTokenCode tc, const TTokenCode *pList) {
+    const TTokenCode *pCode;  // ptr to token code in list
+
+    if (!pList) return 0;
+
+    for (pCode = pList; *pCode; ++pCode) {
+        if (*pCode == tc) return 1;  // in list
+    }
+
+    return 0;  // not in list
+}
--- src/shared/common.c
+++ src/shared/common.c
@@ -13,11 +13,8 @@
 #include <stdio.h>
 #include <common.h>
 #include <symtab.h>
-#include <icode.h>
 #include <cbm.h>
 #include <conio.h>
-#include <types.h>
-#include <icode.h>
 #include <membuf.h>
 
 short cntSymtabs;
@@ -25,108 +22,6 @@ CHUNKNUM firstSymtabChunk;
 CHUNKNUM globalSymtab;
 char isFatalError;
 short currentLineNumber;
-
-// Tokens that can start a declaration
-const TTokenCode tlDeclarationStart[] = {
-    tcCONST, tcTYPE, tcVAR, tcPROCEDURE, tcFUNCTION, tcDummy
-};
-
-// Tokens that can follow a declaration
-const TTokenCode tlDeclarationFollow[] = {
-    tcSemicolon, tcIdentifier, tcDummy
-};
-
-// Tokens that can start an identifier or field
-const TTokenCode tlIdentifierStart[] = {
-    tcIdentifier, tcDummy
-};
-
-// Tokens that can follow an identifier or field
-const TTokenCode tlIdentifierFollow[] = {
-    tcComma, tcIdentifier, tcColon, tcSemicolon, tcDummy
-};
-
-// Tokens that can follow an identifier or field sublist
-const TTokenCode tlSublistFollow[] = {
-    tcColon, tcDummy
-};
-
-// Tokens that can follow a field declaration
-const TTokenCode tlFieldDeclFollow[] = {
-    tcSemicolon, tcIdentifier, tcEND, tcDummy
-};
-
-// Tokens that can start an enumeration constant
-const TTokenCode tlEnumConstStart[] = {
-    tcIdentifier, tcDummy
-};
-
-// Tokens that can follow an enumeration constant
-const TTokenCode tlEnumConstFollow[] = {
-    tcComma, tcIdentifier, tcRParen, tcSemicolon, tcDummy
-};
-
-// Tokens that can follow a subrange limit
-const TTokenCode tlSubrangeLimitFollow[] = {
-    tcDotDot, tcIdentifier, tcPlus, tcMinus, tcString,
-    tcRBracket, tcComma, tcSemicolon, tcOF, tcDummy
-};
-
-// Tokens that can start an index type
-const TTokenCode tlIndexStart[] = {
-    tcIdentifier, tcNumber, tcString, tcLParen, tcPlus, tcMinus,
-    tcDummy
-};
-
-// Tokens that can follow an index type
-const TTokenCode tlIndexFollow[] = {
-    tcComma, tcRBracket, tcOF, tcSemicolon, tcDummy
-};
-
-// Tokens that can follow an index type list
-const TTokenCode tlIndexListFollow[] = {
-    tcOF, tcIdentifier, tcLParen, tcARRAY, tcRECORD,
-    tcPlus, tcMinus, tcNumber, tcString, tcSemicolon, tcDummy
-};
-
-// Tokens that can start a procedure or function definition
-const TTokenCode tlProcFuncStart[] = {
-    tcPROCEDURE, tcFUNCTION, tcDummy
-};
-
-// Tokens that can follow a procedure or function definition
-const TTokenCode tlProcFuncFollow[] = {
-    tcSemicolon, tcDummy
-};
-
-// Tokens that can follow a routine header
-const TTokenCode tlHeaderFollow[] = {
-    tcSemicolon, tcDummy
-};
-
-// Tokens that can follow a function id in a header
-const TTokenCode tlProgProcIdFollow[] = {
-    tcLParen, tcColon, tcSemicolon, tcDummy
-};
-
-// Tokens that can follow a function id in a header
-const TTokenCode tlFuncIdFollow[] = {
-    tcLParen, tcColon, tcSemicolon, tcDummy
-};
-
-// Tokens than can follow an actual variable parameter
-const TTokenCode tlActualVarParmFollow[] = {
-    tcComma, tcRParen, tcDummy
-};
-
-// Tokens that can follow a formal parameter list
-const TTokenCode tlFormalParmsFollow[] = {
-    tcRParen, tcSemicolon, tcDummy
-};
-
-const TTokenCode tlSubscriptOrFieldStart[] = {
-    tcLBracket, tcPeriod, tcDummy
-};
 
 void initCommon(void)
 {
