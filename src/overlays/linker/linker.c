24c72a4a830d9b0d67f3268db2459f0b1b81b5f6
Ken Schenke <kenschenke@gmail.com>
2023-10-21 11:52:05 -0500

    Update linker and compiler tests for Mega 65

--- src/overlays/linker/linker.c
+++ src/overlays/linker/linker.c
@@ -20,6 +20,26 @@
 #define RUNTIME_STACK_SIZE 2048
 
 #ifdef COMPILERTEST
+#ifdef __MEGA65__
+static unsigned char chainCall[] = {
+	LDA_IMMEDIATE, 0,
+	LDX_IMMEDIATE, 8,		// Offset 3: device
+	LDY_IMMEDIATE, 0xff,
+	JSR, 0xba, 0xff,		// SETLFS
+
+	LDA_IMMEDIATE, 0,		// Offset 10: strlen(name)
+	LDX_IMMEDIATE, 0x1c,	// Offset 12: lower str address
+	LDY_IMMEDIATE, 0x7a,	// Offset 14: upper str address
+	JSR, 0xbd, 0xff,		// SETNAM
+
+	LDA_IMMEDIATE, 0,
+	TAX,
+	TAY,
+	JSR, 0xd5, 0xff,		// LOAD
+
+	JMP, 0x11, 0x20,
+};
+#elif defined(__C64__)
 static unsigned char chainCall[] = {
 	LDA_IMMEDIATE, 0,
 	LDX_IMMEDIATE, 8,		// Offset 3: device
@@ -38,18 +58,22 @@ static unsigned char chainCall[] = {
 
 	JMP, 0x0d, 0x08,
 };
+#else
+#error chainCall not defined for this platform
 #endif
+#endif  // end of COMPILERTEST
 
 #ifdef COMPILERTEST
-// static void copyChainCall(char* name);
 static void writeChainCall(char* name);
 #endif
 
+#ifdef __MEGA65__
 #define PRG_HEADER_CODE_OFFSET_1 5
 #define PRG_HEADER_CODE_OFFSET_2 45
 #define PRG_HEADER_CODE_OFFSET_3 47
 #define PRG_HEADER_CODE_OFFSET_4 70
 #define PRG_HEADER_CODE_OFFSET_5 74
+#define PRG_HEADER_LENGTH 77
 static unsigned char prgHeader[] = {
 	// Make a backup copy of page zero
 	LDX_IMMEDIATE, 0,
@@ -69,8 +93,72 @@ static unsigned char prgHeader[] = {
 	LDA_IMMEDIATE, 0,
 	STA_ZEROPAGE, ZP_SPL,
 	STA_ZEROPAGE, ZP_STACKFRAMEL,
-	LDA_IMMEDIATE, 0xd0,
-	STA_ZEROPAGE, ZP_SPH,
+	LDA_IMMEDIATE, 0xc0,		// Runtime stack at $C000
+	STA_ZEROPAGE, ZP_SPH,		// (grows downward)
+	STA_ZEROPAGE, ZP_STACKFRAMEH,
+
+	// Set the runtime stack size and initialize the stack
+	LDA_IMMEDIATE, WORD_LOW(RUNTIME_STACK_SIZE),
+	LDX_IMMEDIATE, WORD_HIGH(RUNTIME_STACK_SIZE),
+	JSR, WORD_LOW(RT_STACKINIT), WORD_HIGH(RT_STACKINIT),
+
+	LDA_IMMEDIATE, WORD_LOW(0xc000 - RUNTIME_STACK_SIZE - 4),
+	LDX_IMMEDIATE, WORD_HIGH(0xc000 - RUNTIME_STACK_SIZE - 4),
+	JSR, WORD_LOW(RT_PUSHAX), WORD_HIGH(RT_PUSHAX),
+	LDA_IMMEDIATE, 0,  // PRG_HEADER_CODE_OFFSET_2
+	LDX_IMMEDIATE, 0,  // PRG_HEADER_CODE_OFFSET_3
+	JSR, WORD_LOW(RT_HEAPINIT), WORD_HIGH(RT_HEAPINIT),
+
+	// Current nesting level
+	LDA_IMMEDIATE, 1,
+	STA_ZEROPAGE, ZP_NESTINGLEVEL,
+
+	// Switch to upper/lower case character set
+	LDA_IMMEDIATE, 0x0e,
+	JSR, WORD_LOW(CHROUT), WORD_HIGH(CHROUT),
+
+	// Disable BASIC ROM
+	LDA_ZEROPAGE, 1,
+	AND_IMMEDIATE, 0xfe,
+	STA_ZEROPAGE, 1,
+
+	// Clear the input buffer
+	JSR, WORD_LOW(RT_CLRINPUT), WORD_HIGH(RT_CLRINPUT),
+
+	// Initialize the int buffer
+	LDA_IMMEDIATE, 0,  // PRG_HEADER_CODE_OFFSET_4
+	STA_ZEROPAGE, ZP_INTPTR,
+	LDA_IMMEDIATE, 0,  // PRG_HEADER_CODE_OFFSET_5
+	STA_ZEROPAGE, ZP_INTPTR + 1,
+};
+#elif defined(__C64__)
+#define PRG_HEADER_CODE_OFFSET_1 5
+#define PRG_HEADER_CODE_OFFSET_2 45
+#define PRG_HEADER_CODE_OFFSET_3 47
+#define PRG_HEADER_CODE_OFFSET_4 70
+#define PRG_HEADER_CODE_OFFSET_5 74
+#define PRG_HEADER_LENGTH 77
+static unsigned char prgHeader[] = {
+	// Make a backup copy of page zero
+	LDX_IMMEDIATE, 0,
+	LDA_X_INDEXED_ZP, 0x02,
+	STA_ABSOLUTEX, 0, 0,  // PRG_HEADER_CODE_OFFSET_1
+	INX,
+	CPX_IMMEDIATE, 0x1b,
+	BNE, 0xf6,
+
+	// Save the stack pointer
+	TSX,
+	STX_ZEROPAGE, ZP_SAVEDSTACK,
+
+	JSR, WORD_LOW(RT_ERRORINIT), WORD_HIGH(RT_ERRORINIT),
+
+	// Initialize runtime stack
+	LDA_IMMEDIATE, 0,
+	STA_ZEROPAGE, ZP_SPL,
+	STA_ZEROPAGE, ZP_STACKFRAMEL,
+	LDA_IMMEDIATE, 0xd0,			// Runtime stack at $D000
+	STA_ZEROPAGE, ZP_SPH,			// (grows downward)
 	STA_ZEROPAGE, ZP_STACKFRAMEH,
 
 	// Set the runtime stack size and initialize the stack
@@ -107,8 +195,12 @@ static unsigned char prgHeader[] = {
 	LDA_IMMEDIATE, 0,  // PRG_HEADER_CODE_OFFSET_5
 	STA_ZEROPAGE, ZP_INTPTR + 1,
 };
+#else
+#error Program header and footer not defined for this platform
+#endif
 
 #define PRG_CLEANUP_OFFSET 12
+#define PRG_CLEANUP_LENGTH 21
 static unsigned char prgCleanup[] = {
 	// Clean up the program's stack frame
 	JSR, WORD_LOW(RT_STACKCLEANUP), WORD_HIGH(RT_STACKCLEANUP),
@@ -133,6 +225,26 @@ static unsigned char prgCleanup[] = {
 #define CHAIN_CODE_CALLL 8
 #define CHAIN_CODE_CALLH 12
 #define CHAIN_CODE_STRLEN 24
+#ifdef __MEGA65__
+static unsigned char chainCode[] = {
+	LDA_IMMEDIATE, 0,
+	LDX_IMMEDIATE, 0,
+	JSR, WORD_LOW(RT_PRINTZ), WORD_HIGH(RT_PRINTZ),
+
+	LDA_IMMEDIATE, 0,
+	STA_ZEROPAGE, ZP_PTR2L,
+	LDA_IMMEDIATE, 0,
+	STA_ZEROPAGE, ZP_PTR2H,
+	LDA_IMMEDIATE, 0,
+	STA_ZEROPAGE, ZP_PTR1L,
+	LDA_IMMEDIATE, 0x7a,
+	STA_ZEROPAGE, ZP_PTR1H,
+	LDA_IMMEDIATE, 0,
+	LDX_IMMEDIATE, 0,
+	JSR, WORD_LOW(RT_MEMCOPY), WORD_HIGH(RT_MEMCOPY),
+	JMP, 0, 0x7a,
+};
+#elif defined (__C64__)
 static unsigned char chainCode[] = {
 	LDA_IMMEDIATE, 0,
 	LDX_IMMEDIATE, 0,
@@ -151,7 +263,10 @@ static unsigned char chainCode[] = {
 	JSR, WORD_LOW(RT_MEMCOPY), WORD_HIGH(RT_MEMCOPY),
 	JMP, 0, 0xca,
 };
+#else
+#error chainCode not defined for this platform
 #endif
+#endif  // end of COMPILERTEST
 
 static void dumpStringLiterals(void);
 static void freeStringLiterals(void);
@@ -203,6 +318,45 @@ static void genExeHeader(void)
 {
 	char startAddr[5], buf[2];
 
+#ifdef __MEGA65__
+	// Link to next line
+	buf[0] = WORD_LOW(codeBase + 14);
+	buf[1] = WORD_HIGH(codeBase + 14);
+	writeToMemBuf(codeBuf, buf, 2);
+
+	// Line number
+	buf[0] = 10;
+	buf[1] = 0;
+	writeToMemBuf(codeBuf, buf, 2);
+
+	// BANK token
+	buf[0] = 0xfe;
+	buf[1] = 0x02;
+	writeToMemBuf(codeBuf, buf, 2);
+
+	// BANK argument
+	buf[0] = '0';
+	buf[1] = ':';
+	writeToMemBuf(codeBuf, buf, 2);
+
+	// SYS token
+	buf[0] = 0x9e;  // SYS
+	writeToMemBuf(codeBuf, buf, 1);
+
+	// Starting address of code
+	sprintf(startAddr, "%04d", codeBase + 16);
+	writeToMemBuf(codeBuf, startAddr, 4);
+
+	// End of BASIC line
+	buf[0] = 0;
+	writeToMemBuf(codeBuf, buf, 1);
+
+	// End of BASIC program marker
+	buf[1] = 0;
+	writeToMemBuf(codeBuf, buf, 2);
+
+	codeOffset = 16;
+#elif defined(__C64__)
 	// Link to next line
 	buf[0] = WORD_LOW(codeBase + 10);
 	buf[1] = WORD_HIGH(codeBase + 10);
@@ -229,11 +383,20 @@ static void genExeHeader(void)
 	writeToMemBuf(codeBuf, buf, 2);
 
 	codeOffset = 12;
+#else
+#error Platform Exe header not defined
+#endif
 }
 
 void linkerPreWrite(void)
 {
+#ifdef __MEGA65__
+	codeBase = 0x2001;
+#elif defined(__C64__)
 	codeBase = 0x801;
+#else
+#error Platform start address not defined
+#endif
 	allocMemBuf(&codeBuf);
 	initLinkerSymbolTable();
 
@@ -253,7 +416,7 @@ void linkerPreWrite(void)
 	linkAddressLookup(BSS_HEAPBOTTOM, codeOffset + PRG_HEADER_CODE_OFFSET_3, 0, LINKADDR_HIGH);
 	linkAddressLookup(BSS_INTBUF, codeOffset + PRG_HEADER_CODE_OFFSET_4, 0, LINKADDR_LOW);
 	linkAddressLookup(BSS_INTBUF, codeOffset + PRG_HEADER_CODE_OFFSET_5, 0, LINKADDR_HIGH);
-	writeCodeBuf(prgHeader, 77);
+	writeCodeBuf(prgHeader, PRG_HEADER_LENGTH);
 }
 
 #ifdef COMPILERTEST
@@ -267,7 +430,7 @@ void linkerPostWrite(const char* prgFilename)
 	int i;
 
 	linkAddressLookup(BSS_ZPBACKUP, codeOffset + PRG_CLEANUP_OFFSET, 0, LINKADDR_BOTH);
-	writeCodeBuf(prgCleanup, 21);
+	writeCodeBuf(prgCleanup, PRG_CLEANUP_LENGTH);
 
 #ifdef COMPILERTEST
 	if (nextTest) {
@@ -333,7 +496,13 @@ void linkerPostWrite(const char* prgFilename)
 	out = fopen(prgFilename, "w");
 	ch = 1;
 	fwrite(&ch, 1, 1, out);
-	ch = 8;
+#ifdef __MEGA65__
+	ch = 0x20;	// 0x2001
+#elif defined(__C64__)
+	ch = 8;		// 0x801
+#else
+#error Platform starting address not defined
+#endif
 	fwrite(&ch, 1, 1, out);
 
 	setMemBufPos(codeBuf, 0);
