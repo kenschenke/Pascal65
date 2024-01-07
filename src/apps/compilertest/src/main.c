8b154b820883616d3d25a8f35522e011276a8363
Ken Schenke <kenschenke@gmail.com>
2024-01-07 10:08:37 -0600

    Update compiler tests for new resolve/typecheck overlays

--- src/apps/compilertest/src/main.c
+++ src/apps/compilertest/src/main.c
@@ -39,13 +39,14 @@ extern void _OVERLAY2_LOAD__[], _OVERLAY2_SIZE__[];
 extern void _OVERLAY3_LOAD__[], _OVERLAY3_SIZE__[];
 extern void _OVERLAY4_LOAD__[], _OVERLAY4_SIZE__[];
 extern void _OVERLAY5_LOAD__[], _OVERLAY5_SIZE__[];
+extern void _OVERLAY6_LOAD__[], _OVERLAY6_SIZE__[];
 unsigned char loadfile(const char *name);
 
 #ifndef __MEGA65__
 static unsigned overlay1size, overlay2size, overlay3size, overlay4size, overlay5size;
 static unsigned overlay1blocks, overlay2blocks, overlay3blocks, overlay4blocks, overlay5blocks;
 
-static BLOCKNUM objcodeCache, linkerCache, parserCache, semanticCache, tokenizerCache;
+static BLOCKNUM objcodeCache, linkerCache, parserCache, resolverCache, tokenizerCache, typecheckCache;
 
 static void loadOverlayFromCache(unsigned size, void *buffer, unsigned cache) {
     struct em_copy emc;
@@ -76,6 +77,40 @@ static void loadOverlayFromFile(char *name, unsigned size, void *buffer, unsigne
 }
 #endif
 
+static void tokenizeAndParseUnits(void)
+{
+    char any, filename[16 + 1];
+    struct unit _unit;
+    CHUNKNUM chunkNum, unitTokens;
+
+    while (1) {
+        chunkNum = units;
+        any = 0;
+        while (chunkNum) {
+            retrieveChunk(chunkNum, &_unit);
+            if (!_unit.astRoot) {
+                any = 1;
+                strcpy(filename, _unit.name);
+                strcat(filename, ".pas");
+
+                loadfile("compilertest.1");
+                unitTokens = tokenize(filename);
+
+                loadfile("compilertest.2");
+                _unit.astRoot = parse(unitTokens);
+                freeMemBuf(unitTokens);
+                storeChunk(chunkNum, &_unit);
+            }
+
+            chunkNum = _unit.next;
+        }
+
+        if (!any) {
+            break;
+        }
+    }
+}
+
 void main()
 {
     int i = 0, avail;
@@ -88,11 +123,13 @@ void main()
     overlay3size = (unsigned)_OVERLAY3_SIZE__;
     overlay4size = (unsigned)_OVERLAY4_SIZE__;
     overlay5size = (unsigned)_OVERLAY5_SIZE__;
+    overlay6size = (unsigned)_OVERLAY6_SIZE__;
     overlay1blocks = overlay1size/BLOCK_LEN + (overlay1size % BLOCK_LEN ? 1 : 0);
     overlay2blocks = overlay2size/BLOCK_LEN + (overlay2size % BLOCK_LEN ? 1 : 0);
     overlay3blocks = overlay3size/BLOCK_LEN + (overlay3size % BLOCK_LEN ? 1 : 0);
     overlay4blocks = overlay4size/BLOCK_LEN + (overlay4size % BLOCK_LEN ? 1 : 0);
     overlay5blocks = overlay5size/BLOCK_LEN + (overlay5size % BLOCK_LEN ? 1 : 0);
+    overlay6blocks = overlay6size/BLOCK_LEN + (overlay6size % BLOCK_LEN ? 1 : 0);
 #endif
 
     initBlockStorage();
@@ -104,9 +141,10 @@ void main()
     // This is done so they don't have to be reloaded for each test.
     if (!allocBlockGroup(&tokenizerCache, overlay1blocks) ||
         !allocBlockGroup(&parserCache, overlay2blocks) ||
-        !allocBlockGroup(&semanticCache, overlay3blocks) ||
-        !allocBlockGroup(&objcodeCache, overlay4blocks) ||
-        !allocBlockGroup(&linkerCache, overlay5blocks)) {
+        !allocBlockGroup(&resolverCache, overlay3blocks) ||
+        !allocBlockGroup(&typecheckCache, overlay4blocks) ||
+        !allocBlockGroup(&objcodeCache, overlay5blocks) ||
+        !allocBlockGroup(&linkerCache, overlay6blocks)) {
         printlnz("Unable to allocate extended memory");
         return;
     }
@@ -115,12 +153,14 @@ void main()
     loadOverlayFromFile("compilertest.1", overlay1size, _OVERLAY1_LOAD__, tokenizerCache);
     printlnz("Loading parser overlay");
     loadOverlayFromFile("compilertest.2", overlay2size, _OVERLAY2_LOAD__, parserCache);
-    printlnz("Loading semantic overlay");
-    loadOverlayFromFile("compilertest", overlay3size, _OVERLAY3_LOAD__, semanticCache);
+    printlnz("Loading resolver overlay");
+    loadOverlayFromFile("compilertest.3", overlay3size, _OVERLAY3_LOAD__, resolverCache);
+    printlnz("Loading typecheck overlay");
+    loadOverlayFromFile("compilertest.4", overlay4size, _OVERLAY4_LOAD__, typecheckCache);
     printlnz("Loading objcode overlay");
-    loadOverlayFromFile("compilertest.4", overlay4size, _OVERLAY4_LOAD__, objcodeCache);
+    loadOverlayFromFile("compilertest.5", overlay6size, _OVERLAY6_LOAD__, objcodeCache);
     printlnz("Loading linker overlay");
-    loadOverlayFromFile("compilertest.5", overlay5size, _OVERLAY5_LOAD__, linkerCache);
+    loadOverlayFromFile("compilertest.6", overlay6size, _OVERLAY6_LOAD__, linkerCache);
 #endif
 
 #ifdef __C128__
@@ -133,6 +173,7 @@ void main()
     while (testFiles[i]) {
         char filename[16 + 1];
         CHUNKNUM tokenId, astRoot;
+        short offset = 0;
 
         strcpy(filename, testFiles[i]);
         strcat(filename, ".pas");
@@ -164,24 +205,40 @@ void main()
         astRoot = parse(tokenId);
         freeMemBuf(tokenId);
 
+        tokenizeAndParseUnits();
+
 #ifndef __MEGA65__
-        loadOverlayFromCache(overlay3size, _OVERLAY3_LOAD__, semanticCache);
+        loadOverlayFromCache(overlay3size, _OVERLAY3_LOAD__, resolverCache);
 #else
     if (!loadfile("compilertest.3")) {
         printlnz("Unable to load overlay from disk");
         exit(0);
     }
 #endif
-        printz("S");
+        printz("R");
         init_scope_stack();
+        resolve_units();
         decl_resolve(astRoot, 0);
-        set_decl_offsets(astRoot, 0, 0);
-        decl_typecheck(astRoot);
+        offset = set_decl_offsets(astRoot, 0, 0);
+        set_unit_offsets(units, offset);
+        fix_global_offsets(astRoot);
 
 #ifndef __MEGA65__
-        loadOverlayFromCache(overlay5size, _OVERLAY5_LOAD__, linkerCache);
+        loadOverlayFromCache(overlay4size, _OVERLAY4_LOAD__, typecheckCache);
 #else
-    if (!loadfile("compilertest.5")) {
+    if (!loadfile("compilertest.4")) {
+        printlnz("Unable to load overlay from disk");
+        exit(0);
+    }
+#endif
+        printz("T");
+        decl_typecheck(astRoot);
+        typecheck_units();
+
+#ifndef __MEGA65__
+        loadOverlayFromCache(overlay6size, _OVERLAY6_LOAD__, linkerCache);
+#else
+    if (!loadfile("compilertest.6")) {
         printlnz("Unable to load overlay from disk");
         exit(0);
     }
@@ -189,9 +246,9 @@ void main()
         printz("W");
         linkerPreWrite();
 #ifndef __MEGA65__
-        loadOverlayFromCache(overlay4size, _OVERLAY4_LOAD__, objcodeCache);
+        loadOverlayFromCache(overlay5size, _OVERLAY5_LOAD__, objcodeCache);
 #else
-    if (!loadfile("compilertest.4")) {
+    if (!loadfile("compilertest.5")) {
         printlnz("Unable to load overlay from disk");
         exit(0);
     }
@@ -199,9 +256,9 @@ void main()
         printz("W");
         objCodeWrite(astRoot);
 #ifndef __MEGA65__
-        loadOverlayFromCache(overlay5size, _OVERLAY5_LOAD__, linkerCache);
+        loadOverlayFromCache(overlay6size, _OVERLAY6_LOAD__, linkerCache);
 #else
-    if (!loadfile("compilertest.5")) {
+    if (!loadfile("compilertest.6")) {
         printlnz("Unable to load overlay from disk");
         exit(0);
     }
